9d8904d6c532f80e764922fee4deb304dcb32073
wangyang <wangyang@sb.com>
2018-08-10 00:01:42 +0800

    center-1.5

--- resources/views/center/index.blade.php
+++ resources/views/center/index.blade.php
@@ -11,7 +11,7 @@
     <!-- Bootstrap -->
     <link href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
     <!-- 私有 -->
-    <link href="{{URL::asset('css/index.css?v=1.3')}}" rel="stylesheet">
+    <link href="{{URL::asset('css/index.css?v=1.4')}}" rel="stylesheet">
     <!-- HTML5 shim 和 Respond.js 是为了让 IE8 支持 HTML5 元素和媒体查询（media queries）功能 -->
     <!-- 警告：通过 file:// 协议（就是直接将 html 页面拖拽到浏览器中）访问页面时 Respond.js 不起作用 -->
     <!--[if lt IE 9]>
@@ -34,49 +34,73 @@
     <ul class="list-group">
         <li class="list-group-item">
             <img src="{{URL::asset('images/p1_02.png')}}" alt="头像" class="img-rounded header-img pull-left">
-            <span>账户充值(余额：18.26元)</span>
-            <span>充值优惠</span>
+            <span class="pull-left img-left font-1">账户充值(<span class="font-2">余额：18.261元</span>)</span>
+            <span class="pull-right img-right font-2">充值优惠<i class="hint-img"></i></span>
         </li>
         <li class="list-group-item item-hr"><hr></li>
         <li class="list-group-item">
             <img src="{{URL::asset('images/p1_04.png')}}" class="pull-left">
-            <span class="pull-left">我的充电记录</span>
+            <span class="pull-left img-left font-1">我的充电记录</span>
         </li>
     </ul>
     <ul class="list-group">
         <li class="list-group-item">
             <img src="{{URL::asset('images/p1_05.png')}}" class="pull-left">
-            <span class="pull-left">电卡充值</span>
-            <span class="pull-right">多充多送</span>
+            <span class="pull-left img-left font-1">电卡充值</span>
+            <span class="pull-right img-right font-2">多充多送<i class="hint-img"></i></span>
         </li>
         <li class="list-group-item item-hr"><hr></li>
         <li class="list-group-item">
             <img src="{{URL::asset('images/p1_06.png')}}" class="pull-left">
-            <span class="pull-left">正在充电</span>
+            <span class="pull-left img-left font-1">正在充电</span>
         </li>
         <li class="list-group-item item-hr"><hr></li>
         <li class="list-group-item">
             <img src="{{URL::asset('images/p1_07.png')}}" class="pull-left">
-            <span class="pull-left">附近电站</span>
+            <span class="pull-left img-left font-1">附近电站</span>
         </li>
     </ul>
     <ul class="list-group">
         <li class="list-group-item">
             <img src="{{URL::asset('images/p1_08.png')}}" class="pull-left">
-            <span class="pull-left">运营商管理平台</span>
+            <span class="pull-left img-left font-1">运营商管理平台</span>
         </li>
         <li class="list-group-item item-hr"><hr></li>
         <li class="list-group-item">
             <img src="{{URL::asset('images/p1_09.png')}}" class="pull-left">
-            <span class="pull-left">诚邀合伙人加盟</span>
-            <span class="pull-right">合作方式丰富多样</span>
+            <span class="pull-left img-left font-1">诚邀合伙人加盟</span>
+            <span class="pull-right img-right font-2">合作方式丰富多样<i class="hint-img"></i></span>
         </li>
         <li class="list-group-item item-hr"><hr></li>
         <li class="list-group-item">
-            <img src="{{URL::asset('images/p1_10.png')}}">
-            <span>关于</span>
+            <img src="{{URL::asset('images/p1_10.png')}}" class="pull-left">
+            <span class="pull-left img-left font-1">关于</span>
         </li>
     </ul>
+</section>
+<section class="footer">
+    <nav class="navbar  navbar-fixed-bottom" role="navigation">
+        <div class="row container">
+            <div class="col-xs-4">
+                <a href="#">
+                    <img src="{{URL::asset('images/p1_11_on.png')}}" class="bottom-img img-responsive center-block">
+                    <span class="text-center center-block font-3">正在充电</span>
+                </a>
+            </div>
+            <div class="col-xs-4">
+                <a href="#">
+                    <img src="{{URL::asset('images/p1_12_off.png')}}" class="bottom-img img-responsive center-block">
+                    <span class="text-center center-block font-3">扫码充电</span>
+                </a>
+            </div>
+            <div class="col-xs-4">
+                <a href="#">
+                    <img src="{{URL::asset('images/p1_13_on.png')}}" class="bottom-img img-responsive center-block">
+                    <span class="text-center center-block font-4">个人中心</span>
+                </a>
+            </div>
+        </div>
+    </nav>
 </section>
 <!-- jQuery (Bootstrap 的所有 JavaScript 插件都依赖 jQuery，所以必须放在前边) -->
 <script src="https://cdn.bootcss.com/jquery/1.12.4/jquery.min.js"></script>
